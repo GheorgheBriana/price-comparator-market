f4bb5d4ba912837eaad063ca3fa265ded255af13
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2025-05-12 20:13:55 +0300

    chore: add loadDiscounts() method in CSVService

--- pricecomparator/src/main/java/com/example/pricecomparator/service/CSVService.java
+++ pricecomparator/src/main/java/com/example/pricecomparator/service/CSVService.java
@@ -1,11 +1,14 @@
 package com.example.pricecomparator.service;
 
+import com.example.pricecomparator.models.Discount;
 import com.example.pricecomparator.models.Product;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,4 +58,48 @@ public class CSVService {
 
         return products;
     }
+
+    public List<Discount> loadDiscounts(String filePath) {
+        List<Discount> discounts = new ArrayList<>();
+        InputStream is = getClass().getClassLoader().getResourceAsStream(filePath);
+
+        if (is == null) {
+            System.out.println("File not found: " + filePath);
+            return discounts;
+        }
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            String line;
+            boolean firstLine = true;
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+            while ((line = br.readLine()) != null) {
+                if (firstLine) {
+                    firstLine = false;
+                    continue;
+                }
+
+                String[] fields = line.split(";");
+                if (fields.length < 9) continue;   // corect ar fi 9 coloane pentru modelul tău
+                Discount discount = new Discount(
+                        fields[0],                             // productId
+                        fields[1],                             // productName
+                        fields[2],                             // brand
+                        Double.parseDouble(fields[3]),         // packageQuantity
+                        fields[4],                             // packageUnit
+                        fields[5],                             // productCategory
+                        formatter.parse(fields[6]),            // fromDate
+                        formatter.parse(fields[7]),            // toDate
+                        Double.parseDouble(fields[8])          // percentageOfDiscount
+                );
+                discounts.add(discount);
+            }
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return discounts;
+    }
 }
+
