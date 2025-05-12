1bc4daea574172bbff2b50c2b679a43e309b7dd9
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2025-05-12 20:21:37 +0300

    feat: add DiscountController

---
+++ pricecomparator/src/main/java/com/example/pricecomparator/controller/DiscountsController.java
@@ -0,0 +1,32 @@
+package com.example.pricecomparator.controller;
+
+import java.util.List;
+
+import com.example.pricecomparator.models.Discount;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pricecomparator.service.CSVService;
+
+@RestController
+@RequestMapping("/discounts") // path
+public class DiscountsController {
+    private final CSVService csvService;
+
+    //Constructor
+    public DiscountsController() {
+        this.csvService = new CSVService();
+    }
+
+    // GET /discounts/{store}/{date}
+    @GetMapping("/{store}/{date}")
+    public List<Discount> getAllDiscounts(
+        @PathVariable String store,
+        @PathVariable String date
+    ) {
+        String fileName = String.format("csv/%s_discounts_%s.csv", store, date);
+        return csvService.loadDiscounts(fileName);
+    }
+}
