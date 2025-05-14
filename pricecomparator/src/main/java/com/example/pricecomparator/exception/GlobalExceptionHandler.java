2ab65f31781c22aa9b4079566e0a7d2698987727
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2025-05-14 12:49:07 +0300

    feat: add global exception handler

---
+++ pricecomparator/src/main/java/com/example/pricecomparator/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.pricecomparator.exception;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllExceptions(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
