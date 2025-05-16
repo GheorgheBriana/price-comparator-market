1ad72e9f12e9162d23b971d49716656f4bf218e7
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2025-05-16 13:50:13 +0300

    test: add CSVServiceTest

---
+++ pricecomparator/src/test/java/com/example/pricecomparator/service/CSVServiceTest.java
@@ -0,0 +1,21 @@
+package com.example.pricecomparator.service;
+
+import java.util.List;
+//maine pun comentarii in csv service si test service
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.pricecomparator.models.Product;
+
+public class CSVServiceTest {
+    private final CSVService csvService = new CSVService();
+
+    @Test
+    void testLoadProducts() {
+        List<Product> products = csvService.loadProducts("csv/lidl_2025-05-01.csv");
+        assertNotNull(products);
+        assertFalse(products.isEmpty());
+        assertTrue(products.size() > 5);
+    }
+
+}
