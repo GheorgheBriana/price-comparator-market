46215737a3d36abbd80a4db06391731631af1198
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2025-05-16 17:49:49 +0300

    feat: add input validation for store and date in BestDiscountsService

--- pricecomparator/src/main/java/com/example/pricecomparator/service/BestDiscountsService.java
+++ pricecomparator/src/main/java/com/example/pricecomparator/service/BestDiscountsService.java
@@ -1,5 +1,7 @@
 package com.example.pricecomparator.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -20,23 +22,42 @@ public class BestDiscountsService {
     }
 
     public List<Discount> getBestDiscounts(String directoryPath, String store, String date) {
-        List<String> fileNames = fileService.getFileNames(directoryPath, store, date);
+        
+        if(store == null || store.isBlank()) {
+            throw new IllegalArgumentException("Store name is invalid");
+        }
+        
+        if(!isValidDate(date)) {
+            throw new IllegalArgumentException("Invalid date. Use the YYYY-MM-DD format");
+        }
 
+        // search all CSV files for a specific store and date
+        String storePattern = store + "_discounts"; // we'll use this variable to only serch for the files that have "_discounts" in their name
+        List<String> fileNames = fileService.getFileNames(directoryPath, storePattern, date);
+
+        // verify if files were found
         if(fileNames.isEmpty()) {
-            throw new RuntimeException("No files found for the specified store and date.");
+            throw new IllegalStateException("There are no discounts for the store: " + store + " on date: " + date);
         }
 
+        // get all the discounts from all files found
         List<Discount> allDiscounts = new ArrayList<>();
-
         for(String fileName : fileNames) {
-            List<Discount> discounts = csvService.loadDiscounts(fileName);
-            allDiscounts.addAll(discounts);
+            allDiscounts.addAll(csvService.loadDiscounts(fileName));
         }
 
+        // sort descending by percentage
         allDiscounts.sort(Comparator.comparingDouble(Discount::getPercentageOfDiscount).reversed());
 
         return allDiscounts;
     }
 
-
+    private boolean isValidDate(String date) {
+        try {
+            LocalDate.parse(date);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
 }
