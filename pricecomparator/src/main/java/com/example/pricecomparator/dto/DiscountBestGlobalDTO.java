76a8f0acd87c54a1905b419738e5d1d4f8ac1a5d
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2025-05-17 19:35:12 +0300

    feat: add DiscountBestGlobalDTO

---
+++ pricecomparator/src/main/java/com/example/pricecomparator/dto/DiscountBestGlobalDTO.java
@@ -0,0 +1,23 @@
+// DiscountWithSourceDTO.java
+package com.example.pricecomparator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class DiscountBestGlobalDTO{
+    private String productId;
+    private String productName;
+    private String brand;
+    private double packageQuantity;
+    private String packageUnit;
+    private String productCategory;
+    private Date fromDate;
+    private Date toDate;
+    private double percentageOfDiscount;
+    private String store;
+    private String sourceFile;
+}
