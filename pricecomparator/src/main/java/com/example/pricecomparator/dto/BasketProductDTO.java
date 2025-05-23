1bae51c5bb0587dc423a852620d1cdc0b77414f0
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2025-05-23 18:05:11 +0300

    feat: add BasketProductDTO

---
+++ pricecomparator/src/main/java/com/example/pricecomparator/dto/BasketProductDTO.java
@@ -0,0 +1,19 @@
+// DTO used in the response for an optimised basket
+// represents a specific product included in the shopping list of a store
+
+package com.example.pricecomparator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BasketProductDTO {
+    private String productId;
+    private String productName;
+    private double unitPrice;
+    private int quantity;
+    private double totalPrice;
+}
